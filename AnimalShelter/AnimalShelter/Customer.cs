981e8ca53880357b4432d17dee9c85e524396e43
jhlmars <jhlmars@gmail.com>
2019-01-10 18:49:23 +0900

    인프런 C# 8강 중간
    인프런 C# 8강 중간

---
+++ AnimalShelter/AnimalShelter/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter
+{
+    class Customer
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string Address;
+        public string Description;
+
+        public Customer(string firstname, string lastname, int age)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Age = age;
+        }
+    }
+}
